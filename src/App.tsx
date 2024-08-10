25de4923e210696df2faaac65c5a2a9ceab84ec9
dousdogukan <dousdogukan@gmail.com>
2024-08-10 21:49:25 +0330

    feat: implement Button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
+import Button from './components/Button';
+
 function App() {
   return (
     <>
       <h1>Wagent landing</h1>
+      <div className="w-full flex space-x-3 p-3">
+        <Button variant="bordered" text="Launch App" />
+        <Button variant="simpleWhite" text="Learn more" />
+        <Button variant="simpleDark" text="Launch app" />
+        <Button variant="simpleGreen" text="Our Team" />
+        <Button variant="basicBlack" text="Request Access" />
+      </div>
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        'white-shadow': '0px 3px 0px 0px #F3F3F3',
+        'dark-shadow':
+          'inset #454d44 0px -1.4px 0px, inset #454d44 0px 2px 0px',
+        'green-shadow': 'inset #464A46 0px -3px 0px',
+      },
+    },
   },
   plugins: [],
 };
