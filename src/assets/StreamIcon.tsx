01955cc7007426718fdbb2f036b6d1a549b3395b
dousdogukan <dousdogukan@gmail.com>
2024-08-14 02:40:35 +0330

    chore: implement Box component

---
+++ src/assets/StreamIcon.tsx
@@ -0,0 +1,39 @@
+const StreamIcon = () => {
+  return (
+    <svg
+      width="67"
+      height="37"
+      viewBox="0 0 67 37"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <rect
+        width="67"
+        height="37"
+        rx="18.5"
+        fill="#B600D4"
+        fill-opacity="0.08"
+      />
+      <path
+        d="M27.3359 13.8335H37.3359"
+        stroke="#EB00FF"
+        stroke-width="1.5"
+        stroke-linecap="round"
+      />
+      <path
+        d="M29.8359 18H41.5026"
+        stroke="#EB00FF"
+        stroke-width="1.5"
+        stroke-linecap="round"
+      />
+      <path
+        d="M27.3359 22.1665H37.3359"
+        stroke="#EB00FF"
+        stroke-width="1.5"
+        stroke-linecap="round"
+      />
+    </svg>
+  );
+};
+
+export default StreamIcon;
---
+++ src/components/Box/index.tsx
@@ -0,0 +1,38 @@
+import cn from 'classnames';
+
+import SingleIcon from '../../assets/SingleIcon';
+import StreamIcon from '../../assets/streamIcon';
+import VestingIcon from '../../assets/VestingIcon';
+
+export type BoxIconType = 'single' | 'vesting' | 'stream';
+
+interface BoxProps {
+  title: string;
+  subTitle: string;
+  icon: BoxIconType;
+  className?: string;
+}
+
+const Box = ({ title, subTitle, icon, className }: BoxProps) => {
+  let icons = <StreamIcon />;
+  if (icon === 'single') {
+    icons = <SingleIcon />;
+  } else if (icon === 'vesting') {
+    icons = <VestingIcon />;
+  }
+  return (
+    <div
+      className={cn(
+        className,
+        'flex-col justify-center items-center text-center bg-white rounded-[20px] py-4 px-[13px] shadow-box-shadow w-[152px] mobile:border mobile:border-1 mobile:border-[#EFEFEF]',
+      )}
+    >
+      <div className="flex justify-center">{icons}</div>
+
+      <p className="mt-[23px] font-medium">{title}</p>
+      <p className="text-[rgba(0, 0, 0, 0.60)]">{subTitle}</p>
+    </div>
+  );
+};
+
+export default Box;
